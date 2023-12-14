93c316d3ed8bdbf2b83d2d31d9acbd7539176419
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2023-12-14 18:42:56 +0700

    admin sidebar header

--- controllers/admin/SiteController.js
+++ controllers/admin/SiteController.js
@@ -4,7 +4,32 @@ class SiteController{
 
     // [GET] /:  
     index(req, res) {
-        res.render('home');
+        res.redirect('./xu_li_don_dat_hang')
     }
+    // [GET] /:  
+    xu_li_don_dat_hang(req, res) {
+        res.render('xu_li_don_dat_hang')
+    }
+    // [GET] /:  
+    nhap_xuat_hang(req, res) {
+        res.render('nhap_xuat_hang')
+    }
+    // [GET] /:  
+    kiem_tra_kho(req, res) {
+        res.render('kiem_tra_kho')
+    }
+    // [GET] /:  
+    bao_cao_doanh_thu(req, res) {
+        res.render('bao_cao_doanh_thu')
+    }
+    // [GET] /:  
+    chi_tieu_cac_mon(req, res) {
+        res.render('chi_tieu_cac_mon')
+    }
+    // [GET] /:  
+    logout(req, res) {
+        res.render('logout')
+    }
+
 }
 module.exports = new SiteController;
