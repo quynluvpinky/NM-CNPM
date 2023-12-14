2fb030d66de42173d98e45a4c7a266422de6c911
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2023-12-14 08:05:01 +0700

    update structure

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
+const router = express.Router();
 // index.js này để set tất cả routes
 const userRouter = require('./user/index')
 const adminRouter = require('./admin/index')
 
 function route(app) {
     // Routes for admin
-    app.use('/admin', (req, res, next) => {
+    router.use('/admin', (req, res, next) => {
         // Thiết lập đường dẫn views riêng cho admin
         app.set("views", './views/admin'); 
         adminRouter(req, res, next);
     });
 
     // Routes for user
-    app.use('/', (req, res, next) => {
+    router.use('/', (req, res, next) => {
         // Thiết lập đường dẫn views riêng cho user
         app.set("views", './views/user'); 
         userRouter(req, res, next);
     });
+    app.use(router);
 }
 module.exports = route; 
