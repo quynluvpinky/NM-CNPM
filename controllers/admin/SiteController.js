ad7aee18562e87d0f44e5c2c767b619b4f724575
21120538 <luuvinhquangqg@gmail.com>
2023-12-20 20:14:51 +0700

    fix2

--- controllers/admin/SiteController.js
+++ controllers/admin/SiteController.js
@@ -1,4 +1,5 @@
 const db = require('../../utilities/db')
+const hanghoaM = require('../../models/hanghoaM')
 
 class SiteController{
 
@@ -7,16 +8,29 @@ class SiteController{
         res.redirect('./xu_li_don_dat_hang')
     }
     // [GET] /:  
-    xu_li_don_dat_hang(req, res) {
-        res.render('xu_li_don_dat_hang')
+    async xu_li_don_dat_hang(req, res, next) {
+        try {
+            const donhang = await hanghoaM.getAllDonHang();
+            res.render('xu_li_don_dat_hang', {donhang: donhang});
+        }
+        catch (e) {
+            next(e);
+        }
     }
     // [GET] /:  
     nhap_xuat_hang(req, res) {
         res.render('nhap_xuat_hang')
     }
     // [GET] /:  
-    kiem_tra_kho(req, res) {
-        res.render('kiem_tra_kho')
+    async kiem_tra_kho(req, res, next) {
+        try {
+            const hangtonkho = await hanghoaM.getSanPhamTonKho();
+            console.log(hangtonkho);
+            res.render('kiem_tra_kho', {hangtonkho: hangtonkho});
+        }
+        catch (e) {
+            next(e);
+        }
     }
     // [GET] /:  
     bao_cao_doanh_thu(req, res) {
