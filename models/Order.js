ef23144831ee7668ba34d9b338d32071980742a1
Nguyen-Nhat <nguyennhat.110120@gmail.com>
2023-12-22 21:24:40 +0700

    sửa lỗi đặt hàng

--- models/Order.js
+++ models/Order.js
@@ -9,12 +9,16 @@ module.exports = class Order{
             INSERT INTO public."${tbName}" (date, clientname, clientphone,order_status)
             VALUES ($1, $2,$3,$4) RETURNING orderid
         `, [new Date(), order.fullName,order.phoneNumber,'not purchased']);
-        const foodData = order.food.map(item => ({...item, orderid: res.orderid}))
-        const productData = order.product.map(item => ({...item, orderid: res.orderid}));
-        const foodCS = new pgp.helpers.ColumnSet(['itemid', 'quantity', 'orderid'], {table: 'foodorder'})
-        const productCS = new pgp.helpers.ColumnSet(['itemid', 'quantity', 'orderid'], {table: 'productorder'})
-        await db.none(pgp.helpers.insert(foodData,foodCS));
-        await db.none(pgp.helpers.insert(productData, productCS));
+        if(order.food != null){
+            const foodData = order.food.map(item => ({...item, orderid: res.orderid}))
+            const foodCS = new pgp.helpers.ColumnSet(['itemid', 'quantity', 'orderid'], {table: 'foodorder'})
+            await db.none(pgp.helpers.insert(foodData,foodCS));
+        }
+        if(order.product != null){
+            const productData = order.product.map(item => ({...item, orderid: res.orderid}));
+            const productCS = new pgp.helpers.ColumnSet(['itemid', 'quantity', 'orderid'], {table: 'productorder'})
+            await db.none(pgp.helpers.insert(productData, productCS));
+        }
         
     }
 }
